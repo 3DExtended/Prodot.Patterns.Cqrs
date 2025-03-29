e167ec41d2a50af8e173f773eeb25d1e57271457
Peter Esser <me@peter-esser.de>
2025-03-29 18:12:03 +0100

    feat: implement SingleModelBySelectorQuery

---
+++ Prodot.Patterns.Cqrs.EfCore/SingleModelBySelectorQueryHandlerBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq.Expressions;
+
+namespace Prodot.Patterns.Cqrs.EfCore;
+
+public abstract class SingleModelBySelectorQueryHandlerBase<
+    TQuery,
+    TModel,
+    TIdentifier,
+    TIdentifierValue,
+    TContext,
+    TEntity
+> : IQueryHandler<TQuery, TModel>
+    where TQuery : SingleModelBySelectorQuery<TModel, TIdentifier, TIdentifierValue, TQuery>
+    where TModel : ModelBase<TIdentifier, TIdentifierValue>
+    where TIdentifier : Identifier<TIdentifierValue, TIdentifier>, new()
+    where TContext : DbContext
+    where TEntity : class, IIdentifiableEntity<TIdentifierValue>
+{
+    private readonly IDbContextFactory<TContext> _contextFactory;
+    private readonly IMapper _mapper;
+
+    protected SingleModelBySelectorQueryHandlerBase(
+        IMapper mapper,
+        IDbContextFactory<TContext> contextFactory
+    )
+    {
+        _mapper = mapper;
+        _contextFactory = contextFactory;
+    }
+
+    public IQueryHandler<TQuery, TModel> Successor { get; set; } = default!;
+
+    public abstract Expression<Func<TEntity, bool>> SelectorPredicate(TQuery query);
+
+    public async Task<Option<TModel>> RunQueryAsync(
+        TQuery query,
+        CancellationToken cancellationToken
+    )
+    {
+        using (
+            var context = await _contextFactory
+                .CreateDbContextAsync(cancellationToken)
+                .ConfigureAwait(false)
+        )
+        {
+            var databaseQuery = AddIncludes(context.Set<TEntity>().AsNoTracking());
+
+            var entity = await databaseQuery
+                .FirstOrDefaultAsync(SelectorPredicate(query), cancellationToken)
+                .ConfigureAwait(false);
+
+            return entity == null ? Option.None : Option.From(_mapper.Map<TModel>(entity));
+        }
+    }
+
+    /// <summary>
+    /// Override this method to add '.Include(...)' calls for retrieving the entities.
+    /// </summary>
+    protected virtual IQueryable<TEntity> AddIncludes(IQueryable<TEntity> queryable) => queryable;
+}
